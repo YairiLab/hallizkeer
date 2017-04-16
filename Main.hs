775a14fa42e2a72d31ad70c2619cd6485babbffe
hiloki owen <hiroki@yairilab.net>
2017-04-16 21:22:15 +0900

    バージョン管理するファイルを間違えていたのを修正

--- Main.hs
+++ Main.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Data.Int
 import Data.Word
 import Data.Bits
+import qualified Data.ByteString.Char8 as B
+
+import Database.PostgreSQL.LibPQ
 import Control.Monad
 import Control.Concurrent
 
@@ -8,21 +12,42 @@ import I2C
 
 main :: IO ()
 main = do
+    conn <- connectToPostgres connectionString
+
     withFile i2cFilepath $ \fd -> do
         Right _ <- initIOControl fd i2cAddress
+        putStrLn "start inserting"
         forever $ do
             ns <- forM [0..63] $ readValue fd
-            let line = unwords $ map show ns
-            putStrLn $ "data:" ++ line
-            threadDelay $ 500 * 1000
+            insertData conn ns
+            putStrLn $ show $ sum ns `div` (length ns)
+            threadDelay $ 2000 * 1000
 
-readValue :: FileDesc -> Word8 -> IO Int16
+connectToPostgres :: B.ByteString -> IO Connection
+connectToPostgres connStr = do
+    putStr "start Postgres connection..."
+    conn <- connectdb connStr
+    errorMessage conn >>= \e -> case e of
+        Just "" -> putStrLn "ok"
+        Just e -> B.putStrLn e
+        _ -> error "fatal error!"
+    return conn
+
+insertData :: Connection -> [Int] -> IO (Maybe Result)
+insertData conn ns = exec conn stmt
+    where col = B.pack $ unwords $ map show ns
+          stmt = B.concat ["INSERT INTO sensor_data (data_array) VALUES('", col, "');"]
+
+readValue :: FileDesc -> Word8 -> IO Int
 readValue fd i = do
     Right low  <- getDataAt fd $ offset + 2*i
     Right high <- getDataAt fd $ offset + 2*i + 1
     let l = fromIntegral low
     let h = fromIntegral high
     return (h `shiftL` 8 + l)
+
+connectionString :: B.ByteString
+connectionString = "host=localhost port=5432 user=postgres password=postgres dbname=testdb"
 
 i2cFilepath :: FilePath
 i2cFilepath = "/dev/i2c-1"
