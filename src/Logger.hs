bc24c3bcb0b4d7f031c7a3027b0c2c9ff8304649
Phil Owen <hiroki@yairilab.net>
2018-01-01 17:32:25 +0900

    インデント微修正

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,12 +1,13 @@
 module Logger (writeLog) where
 
-import Control.Applicative       ((<$>))
 import qualified Data.ByteString.Char8 as B
+import Control.Applicative ((<$>))
 import System.Posix.Syslog (Facility(LOCAL5), Priority(Info), withSyslog, defaultConfig)
 
 writeLog :: [Int] -> IO ()
 writeLog ns = writeLog' s
     where s = B.unwords $ (B.pack . show) <$> ns
+
 writeLog' :: B.ByteString -> IO ()
 writeLog' s = withSyslog defaultConfig $ \syslog -> do
     syslog facility Info s
